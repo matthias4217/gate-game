84cf3c26471830870695786123c1064a66014840
Matthias <matthias.goffette@telecom-sudparis.eu>
2018-03-10 11:57:18 +0100

    Removed Library/

--- Assets/_Scripts/Dialogue/Interactable.cs
+++ Assets/_Scripts/Dialogue/Interactable.cs
@@ -8,6 +8,7 @@ using UnityEngine;
  */
 public class Interactable : MonoBehaviour {
 
+    public float distanceTrigger;
     //public TextAsset fichier_dialogue;
     //static Dialogue dialogue = JsonUtility.FromJson<Dialogue>(fichier_dialogue.text);
     /*
@@ -25,6 +26,34 @@ public class Interactable : MonoBehaviour {
 	/*
 	 * Appelle la fonction StartDialogue du DialogueManager avec l'attribut sentences
 	 */
+
+    void Start()
+    {
+       // Nous obtenons la position de l'objet
+       // Puis de Player
+       // Si < x, alors Trigger
+       GameObject player = GameObject.Find ("Player");
+       Transform playerTransform = player.transform;
+       // get player position
+       Vector3 playerPosition = playerTransform.position;
+
+        Vector3 pnjPosition = transform.position;
+
+        Vector3 distanceVector = playerPosition - pnjPosition;
+        float distance = distanceVector.magnitude;
+
+        if (distance < distanceTrigger) {
+            TriggerDialogue();
+        }
+       /*
+        * Ou avec un collider ?
+        */
+
+       /*
+        * Ou une touche à activer
+        */
+    }
+
     public void TriggerDialogue ()
     {
         FindObjectOfType<DialogueManager>().StartDialogue(sentences); 

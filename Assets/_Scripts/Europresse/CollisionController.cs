69952c36b43523ac3795061d6dae1ee0e4ee987e
Raphael Tran <raphael.tran@telecom-sudparis.eu>
2018-03-13 15:06:14 +0100

    je bosse sur europresse

--- Assets/_Scripts/Europresse/CollisionController.cs
+++ Assets/_Scripts/Europresse/CollisionController.cs
@@ -11,11 +11,11 @@ public class CollisionController : MonoBehaviour
     void OnCollisionStay2D(Collision2D other)
     {
         //if player triggers an Enemy and health is greater than 0
-        if (other.gameObject.tag == "Enemy" && !other.gameObject.GetComponent<EnemyController>().isDown && healthBarSlider.value > 0)
+     /*   if (other.gameObject.tag == "Enemy" && !other.gameObject.GetComponent<EnemyController>().isDown && healthBarSlider.value > 0)
         {
             healthBarSlider.value -= 0.002f;  //reduce health
         }
-    }
+    }*/
 
 //    void OnTriggerEnter2D(Collider2D other) //Pour détruire l'ennemi si contact avec pieds du joueur
 //    {
@@ -24,5 +24,5 @@ public class CollisionController : MonoBehaviour
 //            this.GetComponent<Rigidbody2D>().AddForce(new Vector2(0, 10) * 10, ForceMode2D.Impulse);
 //            Destroy(other.gameObject);
 //        }
-//    }
+  }
 }

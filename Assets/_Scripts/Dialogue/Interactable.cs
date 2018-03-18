4a421f0373b91f1dffb61ab1545e242d138872da
Matthias <matthias.goffette@telecom-sudparis.eu>
2018-03-18 19:55:45 +0100

    Nettoyage de Interactable

--- Assets/_Scripts/Dialogue/Interactable.cs
+++ Assets/_Scripts/Dialogue/Interactable.cs
@@ -4,17 +4,11 @@ using UnityEngine;
 
 /* 
  * Ce script s'attache à tout objet avec lequel on peut interagir. 
- * L'objet doit avoir un Collider
+ * L'objet doit avoir un Collider de type Collider2D, ainsi qu'un rigidbody
  */
 public class Interactable : MonoBehaviour {
 
     public TextAsset fichier_dialogue;
-    /*
-     * Une phrase = une boîte de dialogue.
-     */
-    //[Tooltip("La liste des phrases qui vont être dites.")]
-    //[TextArea(3, 100)]
-    //public Queue<string> sentences;
     [TextArea(3, 100)]
     private Queue<string> sentences;
 
@@ -38,34 +32,6 @@ public class Interactable : MonoBehaviour {
 	/*
 	 * Appelle la fonction StartDialogue du DialogueManager avec l'attribut sentences
 	 */
-
-    void Update()
-    {
-       // Nous obtenons la position de l'objet
-       // Puis de Player
-       // Si < x, alors Trigger
-       /* GameObject player = GameObject.Find ("Player");
-        * Transform playerTransform = player.transform;
-        * // get player position
-        * Vector3 playerPosition = playerTransform.position;
-        * 
-        * Vector3 pnjPosition = transform.position;
-        * 
-        * Vector3 distanceVector = playerPosition - pnjPosition;
-        * float distance = distanceVector.magnitude;
-        * 
-        * if (distance < distanceTrigger) {
-        *    TriggerDialogue();
-        * }
-        */
-       /*
-        * Ou avec un collider ?
-        */
-
-       /*
-        * Ou une touche à activer
-        */
-    }
 
     public void TriggerDialogue ()
     {

6e98a40a863715dcd2335b0461b524bef0598713
armelle.boussaid <armelle.boussaid@telecom-sudparis.eu>
2018-03-24 09:55:28 +0100

    o

---
+++ Assets/_Scripts/Dialogue/DialogueManagerTest.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManagerTest : MonoBehaviour {
+	public DialogueTextTest dialogue;
+	public Text NameText;
+	public Text DialogueText;
+	public Queue<string> sentences;
+
+	void Start () {
+		sentences = new Queue<string> ();
+	}
+
+	public void TriggerDialogue() {
+		StartDialogue (dialogue);
+	}
+
+	public void StartDialogue (DialogueTextTest dialogue) {
+		NameText.text = dialogue.NPCName;
+		sentences.Clear ();
+		foreach (string sentence in dialogue.sentences) {
+			sentences.Enqueue (sentence);
+		}
+		DisplayNextSentence ();
+	}
+
+	public void DisplayNextSentence() {
+		if (sentences.Count == 0) {
+			return;
+		}
+		string sentence = sentences.Dequeue ();
+		StopAllCoroutines ();
+		StartCoroutine (TypeSentence (sentence));
+	}
+
+	IEnumerator TypeSentence (string sentence) {
+		DialogueText.text = "";
+		foreach (char letter in sentence.ToCharArray()) 
+		{
+			DialogueText.text += letter;
+			yield return null;
+		}
+	}
+}
---
+++ Assets/_Scripts/Dialogue/DialogueTextTest.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+	[System.Serializable]
+
+//Cette classe permet de préciser le contenu textuel du dialogue d'un NPC
+	public class DialogueTextTest {
+
+		public string NPCName;	// The name of the character saying this dialogue	
+		[TextArea(3, 100)]
+	public string[] sentences;
+}

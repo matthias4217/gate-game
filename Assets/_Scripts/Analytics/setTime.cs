cfae215b5a6e2cd3e0c8fe08c53019d323f31019
Matthias <matthias.goffette@telecom-sudparis.eu>
2018-04-17 18:49:07 +0200

    Ajout de la prise en compte du temps dans GA
      * entre le lancement du niveau et le passage du checkpoint

---
+++ Assets/_Scripts/Analytics/setTime.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class setTime : MonoBehaviour {
+
+    public string niveau;
+
+	// Use this for initialization
+	void Start () {
+            PlayerPrefs.SetFloat("time" + niveau, Time.time);		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

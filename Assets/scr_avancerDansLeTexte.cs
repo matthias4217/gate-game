7be06b1cf2500dbbd36c42fc04a10ad41b355a5f
pitchinpingouin <35998664+pitchinpingouin@users.noreply.github.com>
2018-04-23 22:11:47 +0200

    derniers ajouts de tuto dans ScholarVox

--- Assets/scr_avancerDansLeTexte.cs
+++ Assets/scr_avancerDansLeTexte.cs
@@ -5,34 +5,70 @@ using UnityEngine.SceneManagement;
 
 public class scr_avancerDansLeTexte : MonoBehaviour 
 {
-
+	public GameObject tuto;
 	public GameObject texte1;
 	public GameObject texte2;
 	public GameObject texte3;
 	public GameObject texte4;
 	public GameObject image1;
 	public GameObject image2;
-	private int compteur = 0;
+	public int compteur = 0;
 	// Update is called once per frame
-	void Update ()
+	void LateUpdate ()
 	{
-		if (Input.GetKeyDown (KeyCode.Space)) {
-			if (compteur < 0) {
+		if (tag == "porte_1") 
+		{
+			if (Input.GetMouseButtonDown (0)) 
+			{
+				if (compteur < 0) 
+				{
+					texte2.SetActive (false);
+					texte3.SetActive (false);
+					texte1.SetActive (true);
+					compteur = 0;
+					tuto.SetActive (false);
+				}
+
+				else if (compteur == 0) 
+				{
+					texte2.SetActive (true);
+					texte1.SetActive (false);
+					compteur++;
+				} 
+
+				else if (compteur == 1) 
+				{
+					texte3.SetActive (true);
+					texte2.SetActive (false);
+					compteur++;
+					compteur *= -1;
+				}
+			}
+		}
+
+		else if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			if (compteur < 0) 
+			{
 				SceneManager.LoadScene ("Introduction_Combat");
 			}
 
-			if (compteur == 0) {
-
+			if (compteur == 0) 
+			{
 				texte2.SetActive (true);
 				texte1.SetActive (false);
 				compteur++;
-			} else if (compteur == 1) {
+			} 
+			else if (compteur == 1) 
+			{
 				image2.SetActive (true);
 				texte3.SetActive (true);
 				texte2.SetActive (false);
 				image1.SetActive (false);
 				compteur++;
-			} else if (compteur == 2) {
+			} 
+			else if (compteur == 2) 
+			{
 				texte4.SetActive (true);
 				texte3.SetActive (false);
 				compteur++;
---
+++ Assets/scr_tuto_ScolarVox.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scr_tuto_ScolarVox : MonoBehaviour 
+{
+	public GameObject tuto1;
+	public GameObject tuto2;
+
+	public void WhenClickedTuto1_ScholarVox()
+	{
+		tuto1.SetActive(true);
+	}
+
+	public void WhenClickedTuto2_ScholarVox()
+	{
+		tuto2.SetActive(true);
+	}
+}
